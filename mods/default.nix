8e221adda7500f91a33a9f4fda738c537edcc3f1
Faximilie <bix@git.faximili.me>
2026-09-04 20:03:57 +1000

    Add sway --unsupported-gpu to uwsm

--- mods/default.nix
+++ mods/default.nix
@@ -212,9 +212,17 @@
     solaar = {
       enable = true;
       userService.enable = true;
+    uwsm =  {
+    	enable = true;
+      waylandCompositors = {
+        sway = {
+          prettyName = "Sway Nvidia";
+          comment = "Sway compositor with --unsuported-gpu managed by UWSM";
+          binPath = "/run/current-system/sw/bin/sway";
+          extraArgs = ["--unsupported-gpu"];
+        };
+      };
     };
-
-    uwsm.enable = true;
 
     steam = {
       enable = true;
